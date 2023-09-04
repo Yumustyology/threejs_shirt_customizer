802b310b64c5610f8a561d42c641cc463f96b78b
Yusuf Mustahan <yumustyology@gmail.com>
2023-09-04 10:39:15 -0700

    updates

--- frontend/src/configs/config.ts
+++ frontend/src/configs/config.ts
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:5000/api/genImg/dalle",
   },
   production: {
-    backendUrl: "https://yung-dalle-image-api.onrender.com",
+    backendUrl: "https://yung-dalle-image-api.onrender.com/api/genImg/dalle",
   },
 };
 
